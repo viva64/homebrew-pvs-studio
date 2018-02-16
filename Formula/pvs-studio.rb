19632e6118d2f97a0fbc61a5e7f08ed8e6186e0b
Svyatoslav <razmyslov@viva64.com>
2018-02-16 11:11:45 +0300

    Fix test.

--- Formula/pvs-studio.rb
+++ Formula/pvs-studio.rb
@@ -1,8 +1,8 @@
 require File.expand_path("../../Abstract/abstract-pvs-studio", __FILE__)
 
-class PvsStudioBeta < AbstractPvsStudio
-  url "http://files.viva64.com/pvs-studio-6.21.25218.150-macos.tgz"
-  version "6.21.25218.150"
+class PvsStudio < AbstractPvsStudio
+  url "http://files.viva64.com/pvs-studio-6.21.25119.118-macos.tgz"
+  version "6.21.25119.118"
   sha256 "391ef6761c13761e8fed19d653ff4aa0b5c883b34a377ca637899bc0b9e6d0f8"
 end
 
