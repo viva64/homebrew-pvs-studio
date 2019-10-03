71e4324e19d6fe6ccff8596a8545aec23a300cf2
Sergey Larin <cerg2010cerg2010@mail.ru>
2019-10-03 16:14:11 +0300

    Fix blame-notifier dependencies

--- Abstract/abstract-blame-notifier.rb
+++ Abstract/abstract-blame-notifier.rb
@@ -3,11 +3,10 @@ class AbstractBlameNotifier < Formula
        "code that triggered certain warnings, based on blame output from version control system." +
        "blame-notifier will send html messages to these developers and also prepare a complete report for \"special\"\n" +
        "persons with the warnings sorted by the \"guilty\" developers.\n" +
-       "Supported version control systems are: git, svn, mercurial."
+       "Supported version control systems are: git, svn, mercurial.\n" +
+       ".NET Core version 3 required to use this package.\n" +
+       "You can install it with: brew cask install dotnet-sdk"
   homepage "https://www.viva64.com/en/pvs-studio/"
-
-  # dotnet core runtime 3 isn't packaged
-  depends_on cask: "dotnet-sdk"
 
   #test do
   #  system "pvs-studio-analyzer", "-h"
