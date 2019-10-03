506eb15162918195ffc69d842c6c2e53038489a4
Svyatoslav Razmyslov <razmyslov@viva64.com>
2019-10-03 15:42:09 +0300

    Upload PVS-Studio Beta: JBN:387

--- Formula/blame-notifier-beta.rb
+++ Formula/blame-notifier-beta.rb
@@ -1,7 +1,7 @@
 require File.expand_path("../../Abstract/abstract-blame-notifier", __FILE__)
 
 class BlameNotifierBeta < AbstractBlameNotifier
-  url "http://files.viva64.com/beta/"
-  version ""
-  sha256 "40ec411006e900864a380d8d98e7fb98148b62273162320dcd98756e99a3178d"
+  url "http://files.viva64.com/beta/blame-notifier-7.04.34476.29-macos.tar.gz"
+  version "7.04.34476.29"
+  sha256 "ed76ac786bba3eac68ff87a65ecac827568dd39cdaa0f8c5fdc75c2fdd29588b"
 end
