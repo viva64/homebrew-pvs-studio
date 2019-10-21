f8c48818649fdf77e7cd36a1bc0fd54d24ac78f9
Svyatoslav Razmyslov <razmyslov@viva64.com>
2019-10-21 09:21:23 +0300

    Upload PVS-Studio Beta: JBN:402

--- Formula/blame-notifier-beta.rb
+++ Formula/blame-notifier-beta.rb
@@ -1,7 +1,7 @@
 require File.expand_path("../../Abstract/abstract-blame-notifier", __FILE__)
 
 class BlameNotifierBeta < AbstractBlameNotifier
-  url "http://files.viva64.com/beta/blame-notifier-7.04.34613.49-macos.tar.gz"
-  version "7.04.34613.49"
-  sha256 "a62f4ac4605939a5d59c8a39812e12336923ac9463b73fa6b3cb4159aa9adcdc"
+  url "http://files.viva64.com/beta/blame-notifier-7.04.34613.51-macos.tar.gz"
+  version "7.04.34613.51"
+  sha256 "c9d7130de620b40357d60ff0420c443ae069b228d0672c144836021dc31e6ea0"
 end
--- Formula/pvs-studio-beta.rb
+++ Formula/pvs-studio-beta.rb
@@ -1,7 +1,7 @@
 require File.expand_path("../../Abstract/abstract-pvs-studio", __FILE__)
 
 class PvsStudioBeta < AbstractPvsStudio
-  url "http://files.viva64.com/beta/pvs-studio-7.04.34835.244-macos.tgz"
-  version "7.04.34835.244"
-  sha256 "2df0a4b2b7a0564cc5154c87902279f053120267828e84d38f039014ef0a8fff"
+  url "http://files.viva64.com/beta/pvs-studio-7.04.34835.246-macos.tgz"
+  version "7.04.34835.246"
+  sha256 "9b855231344d132685cd2469803d61ca1bef72863b35161c3580cb0f0d51cd59"
 end
