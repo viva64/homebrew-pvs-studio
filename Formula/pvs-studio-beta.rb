31e5bd86f2e740415628833cb5496e725fba9d7a
Svyatoslav Razmyslov <razmyslov@viva64.com>
2019-10-29 16:53:50 +0300

    Upload PVS-Studio Beta: JBN:410

--- Formula/pvs-studio-beta.rb
+++ Formula/pvs-studio-beta.rb
@@ -1,7 +1,7 @@
 require File.expand_path("../../Abstract/abstract-pvs-studio", __FILE__)
 
 class PvsStudioBeta < AbstractPvsStudio
-  url "http://files.viva64.com/beta/pvs-studio-7.04.34983.268-macos.tgz"
-  version "7.04.34983.268"
-  sha256 "6e230da3125dd7bc650c965880c69eca1a6ecfa0f3599b086f1c5e5ba0648d09"
+  url "http://files.viva64.com/beta/pvs-studio-7.04.34983.270-macos.tgz"
+  version "7.04.34983.270"
+  sha256 "4849f134b05232b5bc23f28ee481cc0409d8d97f2044b74c34bbb514d2443b08"
 end
