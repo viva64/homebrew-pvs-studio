13a5c6b72b344542df4fbf53d437f784fb30c132
Svyatoslav Razmyslov <razmyslov@viva64.com>
2021-08-16 14:58:49 +0300

    Upload PVS-Studio Beta: JBN:655

--- Formula/pvs-studio-dotnet-beta.rb
+++ Formula/pvs-studio-dotnet-beta.rb
@@ -1,7 +1,7 @@
 require File.expand_path("../../Abstract/abstract-pvs-studio-dotnet", __FILE__)
 
 class PvsStudioDotnetBeta < AbstractPvsStudioDotnet
-  url "http://files.viva64.com/beta/pvs-studio-dotnet-7.14.50636.663-macos.tar.gz"
-  version "7.14.50636.663"
-  sha256 "56a83bf505017059945ca7da88c86f095d328c64ce1f1b0ab95d4a1483b9deba"
+  url "http://files.viva64.com/beta/pvs-studio-dotnet-7.14.50693.664-macos.tar.gz"
+  version "7.14.50693.664"
+  sha256 "477e1dcc2fb63f6cb4db7b1ae2445561e82b4b6dd1df627e5a8089675f8130ba"
 end
