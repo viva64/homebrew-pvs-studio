e8c52d5f224b5488eec96385c44f89a53b5c2575
Svyatoslav Razmyslov <razmyslov@viva64.com>
2021-10-26 13:32:56 +0300

    Update pvs-studio.rb

--- Formula/pvs-studio.rb
+++ Formula/pvs-studio.rb
@@ -1,7 +1,7 @@
 require File.expand_path("../../Abstract/abstract-pvs-studio", __FILE__)
 
 class PvsStudio < AbstractPvsStudio
-  url "http://files.pvs-studio.com/pvs-studio-7.15.53134.163-macos.tgz"
-  version "7.15.53134.163"
-  sha256 "8d5a4aa187fe08b4a74c931ae4506114b9033df632bfec591d63fbc300e248fe"
+  url "http://files.pvs-studio.com/pvs-studio-7.15.53844.169-macos.tgz"
+  version "7.15.53844.169"
+  sha256 "330210d17849279656b4a3157e72eecd32cddb09b60ecc3ede5e09b71cd29085"
 end
