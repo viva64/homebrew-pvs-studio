f7869d87be1d7e485b111815d9c6aeb14d072117
Sergey Vasilyev <s.vasiliev94@rambler.ru>
2021-11-26 11:24:08 +0300

    Update abstract-pvs-studio.rb
    Update website

--- Abstract/abstract-pvs-studio.rb
+++ Abstract/abstract-pvs-studio.rb
@@ -2,7 +2,7 @@ class AbstractPvsStudio < Formula
   desc "PVS-Studio is a tool used to detect bugs in the source code of programs written in C, C++ and C#.\n" +
        "PVS-Studio performs static code analysis and generates a report that helps a programmer find and fix bugs.\n" +
        "PVS-Studio does a wide range of code checks; but it is especially useful to search for misprints and Copy-Paste errors."
-  homepage "https://www.viva64.com/en/pvs-studio/"
+  homepage "https://pvs-studio.com/en/pvs-studio/"
 
   def install
     bin.install "bin/pvs-studio", "bin/pvs-studio-analyzer", "bin/plog-converter"
