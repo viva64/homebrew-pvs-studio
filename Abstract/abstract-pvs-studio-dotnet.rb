339fe1b75b2af0bb429ae585210c5f338fac1b17
Sergey Vasilyev <s.vasiliev94@rambler.ru>
2021-11-26 11:23:46 +0300

    Update abstract-pvs-studio-dotnet.rb
    Update website and required .NET SDK version

--- Abstract/abstract-pvs-studio-dotnet.rb
+++ Abstract/abstract-pvs-studio-dotnet.rb
@@ -2,9 +2,9 @@ class AbstractPvsStudioDotnet < Formula
   desc "PVS-Studio is a static code analyzer and SAST (static application security\n" +
        "testing) tool that is available for C and C++ desktop and embedded development,\n" +
        "C# and Java under Windows, Linux and macOS.\n" +
-       ".NET version 5.0 required to use this package.\n" +
+       ".NET version 6.0 required to use this package.\n" +
        "You can install it with: brew cask install dotnet-sdk"
-  homepage "https://www.viva64.com/en/pvs-studio/"
+  homepage "https://pvs-studio.com/en/pvs-studio/"
 
   def install
     bin.install "bin/pvs-studio-dotnet"
