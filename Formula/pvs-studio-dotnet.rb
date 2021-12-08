129b98c5c08b31c64fbfd1c34b92e49e3c0be2c0
Svyatoslav Razmyslov <razmyslov@viva64.com>
2021-12-08 10:15:10 +0300

    Update pvs-studio-dotnet.rb

--- Formula/pvs-studio-dotnet.rb
+++ Formula/pvs-studio-dotnet.rb
@@ -1,7 +1,7 @@
 require File.expand_path("../../Abstract/abstract-pvs-studio-dotnet", __FILE__)
 
 class PvsStudioDotnet < AbstractPvsStudioDotnet
-  url "http://files.pvs-studio.com/pvs-studio-dotnet-7.15.53822.759-macos.tar.gz"
-  version "7.15.53822.759"
-  sha256 "cc3c340a540217541d51a4be6d021679b320a5342ba1ac19504e5fa873e2b86b"
+  url "http://files.pvs-studio.com/pvs-studio-dotnet-7.16.55368.802-macos.tar.gz"
+  version "7.16.55368.802"
+  sha256 "b17060bc290f8d6150dc60f101ecaab24859de6e94cfa7e30a441d089ea77d3c"
 end
