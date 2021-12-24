bdf21ef2ff2ecd242d9f420d147fa99c70c316d9
BORSH <borshpro@users.noreply.github.com>
2021-12-24 21:33:30 +0300

    Update PVS-Studio [7.16.55312.179]
    Moved from insecure `http://files.pvs-studio.com` to secure `https://cdn.pvs-studio.com`

--- Formula/pvs-studio.rb
+++ Formula/pvs-studio.rb
@@ -1,7 +1,7 @@
 require File.expand_path("../../Abstract/abstract-pvs-studio", __FILE__)
 
 class PvsStudio < AbstractPvsStudio
-  url "http://files.pvs-studio.com/pvs-studio-7.16.55312.178-macos.tgz"
-  version "7.16.55312.178"
-  sha256 "667a28eebcd05248b22230597729bb5e02c8c3fe4ad051e5458d0a69fc38820d"
+  url "https://cdn.pvs-studio.com/pvs-studio-7.16.55312.179-macos.tgz"
+  version "7.16.55312.179"
+  sha256 "bb319a4b36a7b96917f61c15c6483e5d720faae803513d5c23d07fe1eddcdc43"
 end
