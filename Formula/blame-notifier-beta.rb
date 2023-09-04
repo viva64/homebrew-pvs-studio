fc078c3b639ed8fd4bc3a9d699f653615f4977de
Svyatoslav Razmyslov <razmyslov@viva64.com>
2023-09-04 12:41:14 +0300

    Upload PVS-Studio Beta: JBN:1009

--- Formula/blame-notifier-beta.rb
+++ Formula/blame-notifier-beta.rb
@@ -1,7 +1,7 @@
 require File.expand_path("../../Abstract/abstract-blame-notifier", __FILE__)
 
 class BlameNotifierBeta < AbstractBlameNotifier
-  url "https://cdn.pvs-studio.com/beta/blame-notifier-7.26.74381.1569-macos.tar.gz"
-  version "7.26.74381.1569"
-  sha256 "173aee001c59a9e05b9db4e027e494e024c13639410b6d371881aa54259d323c"
+  url "https://cdn.pvs-studio.com/beta/blame-notifier-7.26.74432.1570-macos.tar.gz"
+  version "7.26.74432.1570"
+  sha256 "13777179a4d724b6d7d9f5d3e9507fd89493dc8abacc305ffa4cba1862bb68fc"
 end
--- Formula/pvs-studio-dotnet-beta.rb
+++ Formula/pvs-studio-dotnet-beta.rb
@@ -1,7 +1,7 @@
 require File.expand_path("../../Abstract/abstract-pvs-studio-dotnet", __FILE__)
 
 class PvsStudioDotnetBeta < AbstractPvsStudioDotnet
-  url "https://cdn.pvs-studio.com/beta/pvs-studio-dotnet-7.26.74381.1527-macos.tar.gz"
-  version "7.26.74381.1527"
-  sha256 "4570af5e30b7a74bc26f101cb632b1dc977831f866ef107d4009cacbf59cc0f3"
+  url "https://cdn.pvs-studio.com/beta/pvs-studio-dotnet-7.26.74432.1528-macos.tar.gz"
+  version "7.26.74432.1528"
+  sha256 "4dbf5fd144ba2776fbfa35d219df28a91dbe1ad29b78b65bf7c31edcf497b3e4"
 end
