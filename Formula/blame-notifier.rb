bb3ba5613e452c1780d97f374a0fae6291d0b756
Svyatoslav Razmyslov <razmyslov@viva64.com>
2024-06-11 19:57:52 +0300

    Update blame-notifier.rb

--- Formula/blame-notifier.rb
+++ Formula/blame-notifier.rb
@@ -1,7 +1,7 @@
 require File.expand_path("../../Abstract/abstract-blame-notifier", __FILE__)
 
 class BlameNotifier < AbstractBlameNotifier
-  url "https://cdn.pvs-studio.com/blame-notifier-7.30.80678.1819-macos.tar.gz"
-  version "7.30.80678.1819"
-  sha256 "c2884277d8ca1b85b3135329751a0752c9d8a5ac5a08921c7c914344efdc91aa"
+  url "https://cdn.pvs-studio.com/blame-notifier-7.31.81895.1885-macos.tar.gz"
+  version "7.31.81895.1885"
+  sha256 "1469b1faa9002f4e0995c8b91c84d4a6016ca97dcd0405511424a39ba42e6f5f"
 end
