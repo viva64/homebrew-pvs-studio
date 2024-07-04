2b7ee6552fe4bfa3094159b8b40498078eb616f6
Svyatoslav Razmyslov <razmyslov@viva64.com>
2024-07-04 17:24:42 +0300

    Upload PVS-Studio Beta: JBN:149

--- Formula/pvs-studio-beta.rb
+++ Formula/pvs-studio-beta.rb
@@ -1,7 +1,7 @@
 require File.expand_path("../../Abstract/abstract-pvs-studio", __FILE__)
 
 class PvsStudioBeta < AbstractPvsStudio
-  url "https://cdn.pvs-studio.com/beta/pvs-studio-7.31.82441.6709-macos.tgz"
-  version "7.31.82441.6709"
-  sha256 "05d3899e0a14d7ef7ec20b112e89c70669b8cc6538c65db2902afb7a83224d87"
+  url "https://cdn.pvs-studio.com/beta/pvs-studio-7.31.82452.6710-macos.tgz"
+  version "7.31.82452.6710"
+  sha256 "1ca5c5b841328fdce0dcb60ad016681d006a947ae59922c0d69049ed01fe0103"
 end
--- Formula/pvs-studio-dotnet-beta.rb
+++ Formula/pvs-studio-dotnet-beta.rb
@@ -1,7 +1,7 @@
 require File.expand_path("../../Abstract/abstract-pvs-studio-dotnet", __FILE__)
 
 class PvsStudioDotnetBeta < AbstractPvsStudioDotnet
-  url "https://cdn.pvs-studio.com/beta/pvs-studio-dotnet-7.31.82420.1980-macos.tar.gz"
-  version "7.31.82420.1980"
-  sha256 "75bb02205adf9ed4c8b47b27c14321e698c27fcfcf01e6e65350f6bdfb621b37"
+  url "https://cdn.pvs-studio.com/beta/pvs-studio-dotnet-7.31.82452.1981-macos.tar.gz"
+  version "7.31.82452.1981"
+  sha256 "a25902aaf170ef55fdc3d9ae4e8dc7b5c0ae5dc38041148b9cc16112a94040d9"
 end
