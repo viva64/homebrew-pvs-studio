49454c21bbb7eb85cb47f5161975420275b19fb2
Svyatoslav Razmyslov <razmyslov@viva64.com>
2024-11-27 16:02:19 +0300

    Upload PVS-Studio Beta: JBN:6

--- Formula/pvs-studio-beta.rb
+++ Formula/pvs-studio-beta.rb
@@ -5,6 +5,6 @@ class PvsStudioBeta < AbstractPvsStudio
       arm: "https://cdn.pvs-studio.com/beta/pvs-studio-7.33.86781.184-macos-arm64.tgz"
   version intel: "7.33.86781.184",
           arm: "7.33.86781.184"
-  sha256 intel: "754993f3f28e37000d174d97b5b119d1e84973594974f1cee60ca5731b5245c2"
+  sha256 intel: "754993f3f28e37000d174d97b5b119d1e84973594974f1cee60ca5731b5245c2",
          arm: "8d1d844ee8315dd7d3f1b64a4ff0eb3c62ed4e4d050f4e013349b5607e334bda"
 end
--- Formula/pvs-studio-dotnet-beta.rb
+++ Formula/pvs-studio-dotnet-beta.rb
@@ -5,6 +5,6 @@ class PvsStudioDotnetBeta < AbstractPvsStudioDotnet
       arm: "https://cdn.pvs-studio.com/beta/pvs-studio-dotnet-7.33.86775.2207-macos-arm64.tar.gz"
   version intel: "7.33.86775.2207",
           arm: "7.33.86775.2207"
-  sha256 intel: "c323c31881fbf9192ba382c942e739a508ebd1096318e3f2ed39b432603f4d43"
+  sha256 intel: "c323c31881fbf9192ba382c942e739a508ebd1096318e3f2ed39b432603f4d43",
          arm: "7cb2c52505bed122f81b06ddf03b50b6155c3ae897ead35605a17093a826cf5a"
 end
