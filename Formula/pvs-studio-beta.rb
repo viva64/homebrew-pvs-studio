ae03a7d6725c3f0d8e23be3034f68365ea5a30fd
Svyatoslav Razmyslov <razmyslov@viva64.com>
2024-11-27 16:58:57 +0300

    Upload PVS-Studio Beta: JBN:7

--- Formula/pvs-studio-beta.rb
+++ Formula/pvs-studio-beta.rb
@@ -1,10 +1,14 @@
 require File.expand_path("../../Abstract/abstract-pvs-studio", __FILE__)
 
 class PvsStudioBeta < AbstractPvsStudio
-  url intel: "https://cdn.pvs-studio.com/beta/pvs-studio-7.33.86781.184-macos-x86_64.tgz",
-      arm: "https://cdn.pvs-studio.com/beta/pvs-studio-7.33.86781.184-macos-arm64.tgz"
-  version intel: "7.33.86781.184",
-          arm: "7.33.86781.184"
-  sha256 intel: "754993f3f28e37000d174d97b5b119d1e84973594974f1cee60ca5731b5245c2",
-         arm: "8d1d844ee8315dd7d3f1b64a4ff0eb3c62ed4e4d050f4e013349b5607e334bda"
+  on_intel do
+    url "https://cdn.pvs-studio.com/beta/pvs-studio-7.33.86781.184-macos-x86_64.tgz"
+    version intel: "7.33.86781.184",
+    sha256 intel: "754993f3f28e37000d174d97b5b119d1e84973594974f1cee60ca5731b5245c2"
+  end
+  on_arm do
+    url "https://cdn.pvs-studio.com/beta/pvs-studio-7.33.86781.184-macos-arm64.tgz"
+    version intel: "7.33.86781.184",
+    sha256 intel: "8d1d844ee8315dd7d3f1b64a4ff0eb3c62ed4e4d050f4e013349b5607e334bda"
+  end
 end
--- Formula/pvs-studio-dotnet-beta.rb
+++ Formula/pvs-studio-dotnet-beta.rb
@@ -1,10 +1,14 @@
 require File.expand_path("../../Abstract/abstract-pvs-studio-dotnet", __FILE__)
 
 class PvsStudioDotnetBeta < AbstractPvsStudioDotnet
-  url intel: "https://cdn.pvs-studio.com/beta/pvs-studio-dotnet-7.33.86775.2207-macos-x86_64.tar.gz",
-      arm: "https://cdn.pvs-studio.com/beta/pvs-studio-dotnet-7.33.86775.2207-macos-arm64.tar.gz"
-  version intel: "7.33.86775.2207",
-          arm: "7.33.86775.2207"
-  sha256 intel: "c323c31881fbf9192ba382c942e739a508ebd1096318e3f2ed39b432603f4d43",
-         arm: "7cb2c52505bed122f81b06ddf03b50b6155c3ae897ead35605a17093a826cf5a"
+  on_intel do
+    url "https://cdn.pvs-studio.com/beta/pvs-studio-dotnet-7.33.86775.2207-macos-x86_64.tar.gz"
+    version intel: "7.33.86775.2207",
+    sha256 intel: "c323c31881fbf9192ba382c942e739a508ebd1096318e3f2ed39b432603f4d43"
+  end
+  on_arm do
+    url "https://cdn.pvs-studio.com/beta/pvs-studio-dotnet-7.33.86775.2207-macos-arm64.tar.gz"
+    version intel: "7.33.86775.2207",
+    sha256 intel: "7cb2c52505bed122f81b06ddf03b50b6155c3ae897ead35605a17093a826cf5a"
+  end
 end
