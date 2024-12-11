c47f258af2f4f8363a095f089b993835e4fdaf7c
Svyatoslav Razmyslov <razmyslov@viva64.com>
2024-12-11 16:23:31 +0300

    Update blame-notifier.rb

--- Formula/blame-notifier.rb
+++ Formula/blame-notifier.rb
@@ -7,8 +7,8 @@ url "https://cdn.pvs-studio.com/beta/"
     sha256 ""
   end
   on_arm do
-    url "https://cdn.pvs-studio.com/beta/"
-    version ""
-    sha256 ""
+    url "https://cdn.pvs-studio.com/beta/blame-notifier-7.34.87460.2110-macos-arm64.tar.gz"
+    version "7.34.87460.2110"
+    sha256 "afaacb9839cb8b3c4c4f74e5915c3f700d6b04506b6aa19795ae2ef8650f4193"
   end
 end
