fb316d6caa7fb9665317578cb7d08d17bf40c420
pvs-studio-dev <devops@viva64.com>
2025-10-25 14:09:52 +0300

    Update pvs-studio.rb

--- Formula/pvs-studio.rb
+++ Formula/pvs-studio.rb
@@ -2,12 +2,12 @@ require File.expand_path("../../Abstract/abstract-pvs-studio", __FILE__)
 
 class PvsStudio < AbstractPvsStudio
 on_intel do
-url "https://cdn.pvs-studio.com/pvs-studio-7.39.99307.684-macos-x86_64.zip"
+url "https://wcdn.pvs-studio.com/pvs-studio-7.39.99307.684-macos-x86_64.zip"
     version "7.39.99307.684"
     sha256 "e071c092c1d27601090270ed2233f9f0b4b760370e7d46fb50529f616b2960ba"
   end
   on_arm do
-    url "https://cdn.pvs-studio.com/pvs-studio-7.39.99307.684-macos-arm64.zip"
+    url "https://wcdn.pvs-studio.com/pvs-studio-7.39.99307.684-macos-arm64.zip"
     version "7.39.99307.684"
     sha256 "daa1d8b96ca57a80cc5d97bbf1b36fd531f589535e2d8a40adf5b5fcd8ba017d"
   end
