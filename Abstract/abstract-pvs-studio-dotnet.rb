ca1525c726e500fe35f0c890bd868d22a053432b
Mikhail Evtihevich <102743181+Evtihevich@users.noreply.github.com>
2025-12-17 16:33:26 +0300

    Update abstract-pvs-studio-dotnet.rb

--- Abstract/abstract-pvs-studio-dotnet.rb
+++ Abstract/abstract-pvs-studio-dotnet.rb
@@ -2,7 +2,7 @@ class AbstractPvsStudioDotnet < Formula
   desc "PVS-Studio is a static code analyzer and SAST (static application security\n" +
        "testing) tool that is available for C and C++ desktop and embedded development,\n" +
        "C# and Java under Windows, Linux and macOS.\n" +
-       ".NET version 9.0 required to use this package.\n" +
+       ".NET version 10.0 required to use this package.\n" +
        "You can install it with: brew cask install dotnet-sdk"
   homepage "https://pvs-studio.com/en/pvs-studio/"
 
