dec057dbb431104d0d8691d340bd6b1c99e26510
pvs-studio-dev <devops@viva64.com>
2026-01-14 12:59:45 +0300

    Upload PVS-Studio Beta: JBN:31

--- Formula/blame-notifier-beta.rb
+++ Formula/blame-notifier-beta.rb
@@ -3,13 +3,13 @@ require File.expand_path("../../Abstract/abstract-blame-notifier", __FILE__)
 class BlameNotifierBeta < AbstractBlameNotifier
   depends_on "pvs-studio-beta"
   on_intel do
-    url "https://files.pvs-studio.com/beta/blame-notifier-7.40.102041.208-macos-x86_64.zip"
-    version "7.40.102041.208"
-    sha256 "abfb02a3221b69646923cc2a73d6d5cd27815aa0f19f856cbc183dba72a10785"
+    url "https://files.pvs-studio.com/beta/blame-notifier-7.40.102057.49-macos-x86_64.zip"
+    version "7.40.102057.49"
+    sha256 "039d3c0fd28eda769933a1ab58c4417ddc41991bf242573dda1457679f06fb55"
   end
   on_arm do
-    url "https://files.pvs-studio.com/beta/blame-notifier-7.40.102041.208-macos-arm64.zip"
-    version "7.40.102041.208"
-    sha256 "c39f9f8ff432f1c2e2405d58873f86103f4eb14b56f26f1cd98d8735f3eefc6b"
+    url "https://files.pvs-studio.com/beta/blame-notifier-7.40.102057.49-macos-arm64.zip"
+    version "7.40.102057.49"
+    sha256 "6dc63bca9ed25e52169cb085b03d8d222f4bbf3ed714c23e89331d2dd0a5fd1d"
   end
 end
