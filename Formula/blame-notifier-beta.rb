df043fa99969fd8ce5317af81f98529a8a44dac5
pvs-studio-dev <devops@viva64.com>
2026-03-04 09:53:41 +0300

    Upload PVS-Studio Beta: JBN:590

--- Formula/blame-notifier-beta.rb
+++ Formula/blame-notifier-beta.rb
@@ -3,13 +3,13 @@ require File.expand_path("../../Abstract/abstract-blame-notifier", __FILE__)
 class BlameNotifierBeta < AbstractBlameNotifier
   depends_on "pvs-studio-beta"
   on_intel do
-    url "https://files.pvs-studio.com/beta/blame-notifier-7.41.103770.256-macos-x86_64.zip"
-    version "7.41.103770.256"
-    sha256 "d359f2e31ede9469523f933e605e3b96d69a2060f45598552fae44ae5164ed6c"
+    url "https://files.pvs-studio.com/beta/blame-notifier-7.41.103826.257-macos-x86_64.zip"
+    version "7.41.103826.257"
+    sha256 "75d866d616ec9c0cb7f06877530287af3dfd878b12abe5ce88ab3c0ac50b04fe"
   end
   on_arm do
-    url "https://files.pvs-studio.com/beta/blame-notifier-7.41.103770.256-macos-arm64.zip"
-    version "7.41.103770.256"
-    sha256 "e0e0952f9e94cc26fa4ace77a359e7cc24174f425f251d4e816c204bd960b434"
+    url "https://files.pvs-studio.com/beta/blame-notifier-7.41.103826.257-macos-arm64.zip"
+    version "7.41.103826.257"
+    sha256 "1fca72619851f4d4f427748aea89b93e21dda851a9c86b1f8877b717074511c4"
   end
 end
--- Formula/pvs-studio-dotnet-beta.rb
+++ Formula/pvs-studio-dotnet-beta.rb
@@ -3,13 +3,13 @@ require File.expand_path("../../Abstract/abstract-pvs-studio-dotnet", __FILE__)
 class PvsStudioDotnetBeta < AbstractPvsStudioDotnet
   depends_on "pvs-studio-beta"
   on_intel do
-    url "https://files.pvs-studio.com/beta/pvs-studio-dotnet-7.41.103770.256-macos-x86_64.zip"
-    version "7.41.103770.256"
-    sha256 "8e3fe1b85eb02edc95080fd83e12aea8c72f2b4c78e44b56960e8e3b95160e39"
+    url "https://files.pvs-studio.com/beta/pvs-studio-dotnet-7.41.103824.257-macos-x86_64.zip"
+    version "7.41.103824.257"
+    sha256 "a1bd23fd9134a447bed6da997f05f6c0a6143b0ab8a7fad0ca072269feea5b62"
   end
   on_arm do
-    url "https://files.pvs-studio.com/beta/pvs-studio-dotnet-7.41.103770.256-macos-arm64.zip"
-    version "7.41.103770.256"
-    sha256 "dea390a85c779d05c73214a2dc99a2c8e199727532958fe397ee9f389f016e30"
+    url "https://files.pvs-studio.com/beta/pvs-studio-dotnet-7.41.103824.257-macos-arm64.zip"
+    version "7.41.103824.257"
+    sha256 "98324e1239d9c6db0d3549bd7f511a03f1056dc228f7539f402ffac8dd6ca6f9"
   end
 end
