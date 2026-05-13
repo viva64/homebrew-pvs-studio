559432da6885bd36ac855b740c37a047f8e5637d
pvs-studio-dev <devops@viva64.com>
2026-05-13 14:41:29 +0300

    Upload PVS-Studio Beta: JBN:642

--- Formula/blame-notifier-beta.rb
+++ Formula/blame-notifier-beta.rb
@@ -3,13 +3,13 @@ require File.expand_path("../../Abstract/abstract-blame-notifier", __FILE__)
 class BlameNotifierBeta < AbstractBlameNotifier
   depends_on "pvs-studio-beta"
   on_intel do
-    url "https://files.pvs-studio.com/beta/blame-notifier-7.42.106123.325-macos-x86_64.zip"
-    version "7.42.106123.325"
-    sha256 "46a290e6ce5baf260d5e3555898db6fc46963f88dcaeb88de8597e1ae346d7c0"
+    url "https://files.pvs-studio.com/beta/blame-notifier-7.42.106174.326-macos-x86_64.zip"
+    version "7.42.106174.326"
+    sha256 "32cc6e6d1fb546e521925c073283236bc2327bb782e5b97129a5253e470e7a83"
   end
   on_arm do
-    url "https://files.pvs-studio.com/beta/blame-notifier-7.42.106123.325-macos-arm64.zip"
-    version "7.42.106123.325"
-    sha256 "60aa2cf942bbb65dcc549babf9ec629363ac68a7756b1c63a5c56fed2079cc70"
+    url "https://files.pvs-studio.com/beta/blame-notifier-7.42.106174.326-macos-arm64.zip"
+    version "7.42.106174.326"
+    sha256 "96caebf5b4725f8e412a66ece07590ef083145a02755f878c5748ce5b49f3940"
   end
 end
--- Formula/pvs-studio-beta.rb
+++ Formula/pvs-studio-beta.rb
@@ -2,13 +2,13 @@ require File.expand_path("../../Abstract/abstract-pvs-studio", __FILE__)
 
 class PvsStudioBeta < AbstractPvsStudio
   on_intel do
-    url "https://files.pvs-studio.com/beta/pvs-studio-7.42.106123.332-macos-x86_64.zip"
-    version "7.42.106123.332"
-    sha256 "e108c610d58a369da6b221a8d7e338659a51484ada4dae58c318b4b1e9eea0fc"
+    url "https://files.pvs-studio.com/beta/pvs-studio-7.42.106200.792-macos-x86_64.zip"
+    version "7.42.106200.792"
+    sha256 "8193436df7054d871f15cea5a7508dee7f54e6c126f2210d343e5f621edb82e0"
   end
   on_arm do
-    url "https://files.pvs-studio.com/beta/pvs-studio-7.42.106123.332-macos-arm64.zip"
-    version "7.42.106123.332"
-    sha256 "05b93b2aa7f458bff13985f60d120f8c9e0d68ffe1256b6570e58d4dbf48e441"
+    url "https://files.pvs-studio.com/beta/pvs-studio-7.42.106200.792-macos-arm64.zip"
+    version "7.42.106200.792"
+    sha256 "33bac85befa7a1f035d12d9a30111f054421ede0d3aeca10028f6d7b6ab61f1a"
   end
 end
