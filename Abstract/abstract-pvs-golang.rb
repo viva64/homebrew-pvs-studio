953f0aef9523b9f3fccc145cd63fc44bfef8cfa6
Mikhail Evtihevich <evtihevich@viva64.com>
2026-07-22 14:47:29 +0300

    fix description

--- Abstract/abstract-pvs-golang.rb
+++ Abstract/abstract-pvs-golang.rb
@@ -1,6 +1,6 @@
 class AbstractPvsStudioGo < Formula
     desc "PVS-Studio is a static code analyzer and SAST tool that is available for C and C++ desktop and embedded development,\n" +
-       "C#, Java, JavaScript, TypeScript and Go under Windows, Linux and macOS.\n" +
+       "C#, Java, JavaScript, TypeScript and Go under Windows, Linux and macOS.\n"
   homepage "https://pvs-studio.com/en/pvs-studio/"
 
   def install
--- Abstract/abstract-pvs-js.rb
+++ Abstract/abstract-pvs-js.rb
@@ -1,7 +1,7 @@
 class AbstractPvsStudioJs < Formula
   desc "PVS-Studio is a static code analyzer and SAST tool that is available for C and C++ desktop and embedded development,\n" +
-       "C#, Java, JavaScript, TypeScript and Go under Windows, Linux and macOS.\n" +
-
+       "C#, Java, JavaScript, TypeScript and Go under Windows, Linux and macOS.\n" 
+       
   homepage "https://pvs-studio.com/en/pvs-studio/"
 
   def install
