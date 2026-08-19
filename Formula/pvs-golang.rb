8aa97905c804f08790a79ee53ce9795e00dac874
pvs-studio-dev <devops@viva64.com>
2026-08-19 15:33:04 +0300

    Update pvs-golang.rb

--- Formula/pvs-golang.rb
+++ Formula/pvs-golang.rb
@@ -1,10 +1,10 @@
 require File.expand_path("../../Abstract/abstract-pvs-golang", __FILE__)
 
 class PvsGolang < AbstractPvsGolang
-  depends_on "pvs-studio"
+depends_on "pvs-studio"
   on_arm do
-    url 
-    version 
-    sha256 
+    url "https://files.pvs-studio.com/pvs-golang-8.00.98-macos.zip"
+    version "8.00.98"
+    sha256 "d56f2893af3df09688adc3030cacdb452521637b5f5aa10360d21f0fd2422f21"
   end
 end
