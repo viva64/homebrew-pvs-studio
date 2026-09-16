f2685ff50f3aa7a1eadebccefad565daeaf6f6a1
pvs-studio-dev <devops@viva64.com>
2026-09-16 12:57:26 +0300

    Upload PVS-Studio Beta: JBN:723

--- Formula/pvs-golang-beta.rb
+++ Formula/pvs-golang-beta.rb
@@ -3,8 +3,8 @@ require File.expand_path("../../Abstract/abstract-pvs-golang", __FILE__)
 class PvsGolangBeta < AbstractPvsGolang
   depends_on "pvs-studio-beta"
   on_arm do
-    url "https://files.pvs-studio.com/beta/pvs-golang-8.00.258-macos.zip"
-    version "8.00.258"
-    sha256 "55bfe58afde5fc3fdde80b2897efb2a912b5e39b6c3c07b11b0144831ef860ae"
+    url "https://files.pvs-studio.com/beta/pvs-golang-8.00.117-macos.zip"
+    version "8.00.117"
+    sha256 "8b87c210cccb112824c779bcc3852ed76eaafd00a37c727813755aa945980d38"
   end
 end
